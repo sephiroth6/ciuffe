9a9c9ae7f7b34471ffb1edab790f91c6e9fc5442
sephiroth6 <sephiroth.a17@gmail.com>
2011-12-13 18:33:27 +0100

    new script3

--- pushgit.sh
+++ pushgit.sh
@@ -3,17 +3,17 @@
 	git add TrainServer/src/guiSrv/
 	git add TrainServer/src/train/
 	echo "aggiunti file del server"
-	sleep 0.38
+	sleep 0.47
 
 	##git add TrainClient/src/train/
 	git add TrainClient/src/guiCli/
 	echo "aggiunti file del client"
-	sleep 0.38
+	sleep 0.47
 	
 	git add Lib/
 	git add CiuffeLib/src/lib/
 	echo "aggiunti file libreria"
-	sleep 0.38
+	sleep 0.47
 
 	echo -n "Inserire il testo del commit: "
 	read p
